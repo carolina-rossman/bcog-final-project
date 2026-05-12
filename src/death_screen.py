90e37989a24f3b78697f74379e09a73aefd03ccf
a-tran69 <atran69@illinois.edu>
2026-05-12 12:28:12 -0500

    death screen beginning

--- src/death_screen.py
+++ src/death_screen.py
@@ -1,1 +1,9 @@
 import pygame
+
+class Display():
+    screen_size = (800, 600)
+    def __init__(self, screen):
+        pygame.init()
+        my_font = pygame.freetype.SysFont('Arial', 65)
+        text_surface = my_font.render('You Died', False, (0, 0, 0))
+        screen.blit(text_surface, (0, 0))
