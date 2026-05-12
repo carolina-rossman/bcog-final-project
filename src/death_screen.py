7cb9c5042b9c30024056eeb553c636fcdfc7ecbe
a-tran69 <atran69@illinois.edu>
2026-05-12 13:29:50 -0500

    base death_screen

--- src/death_screen.py
+++ src/death_screen.py
@@ -4,31 +4,38 @@ import sys
 
 
 class Display:
-    screen_size = (800, 600)
+    window = pygame.display.set_mode((800, 600))
 
-    def __init__(self, screen):
+    def __init__(self, window):
         pygame.init()
-        font = pygame.freetype.SysFont("Arial", 65)
+        self.font = pygame.freetype.SysFont("Arial", 65, bold=True)
+        self.background = pygame.Surface(window.get_size())
+        self.red = [255, 99, 71]
+        self.background.fill(self.red)
+
         running = True
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
 
-            screen.fill((255, 255, 255))
-            text_surface, rect = font.render("You Died", (0, 0, 0))
-            screen.blit(text_surface, (40, 250))
-            # font.render_to(screen, (40, 350), "You Died", (0, 0, 0))
-
+            window.blit(self.background, (0, 0))
+            text_surface, rect = self.font.render("You Died")
+            window.blit(text_surface, (250, 250))
             pygame.display.flip()
 
         pygame.quit()
 
+    def buttons(self):
+        pygame.draw.rect(self.window, (0, 200, 0), self.restart_button)
+        pygame.draw.rect(self.window, (200, 0, 0), self.quit_button)
+        self.button_font = pygame.freetype.SysFont("Arial", 20, bold=True)
+
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode(Display.screen_size)
-    Display(screen)
+    window = pygame.display.set_mode((800, 600))
+    Display(window)
 
 
 if __name__ == "__main__":
